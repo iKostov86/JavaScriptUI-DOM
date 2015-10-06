bd1dfe2f8143a2163ae40c95da5018a79208e126
iKostov86 <i.kostov86@gmail.com>
2015-10-07 01:50:19 +0300

    rename files

--- Demos/Demos-JS-UI-DOM/WebStorm/SVG/script-resize-svg.js
+++
@@ -1,8 +0,0 @@
-//var container = document.getElementById('svg-container'),
-//    svg = document.getElementById('the-svg');
-
-//svg.setAttribute('width', (svg.getAttribute('clientWidth')).toString());
-//svg.setAttribute('height', (svg.getAttribute('clientHeight')).toString());
-
-//svg.setAttribute('width', (container.clientWidth).toString());
-//svg.setAttribute('height', (container.clientHeight).toString());
