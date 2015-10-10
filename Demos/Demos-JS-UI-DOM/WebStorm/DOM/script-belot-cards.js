b68af1ccb42a28b4c8defe1a0e134d6bdcba7415
Ivaylo <i.kostov86@gmail.com>
2015-10-11 02:26:00 +0300

    belot cards

--- Demos/Demos-JS-UI-DOM/WebStorm/DOM/script-belot-cards.js
+++ Demos/Demos-JS-UI-DOM/WebStorm/DOM/script-belot-cards.js
@@ -1,8 +1,17 @@
 var root = document.createElement('div'),
-    cardsSourse = 'cardTheme2.png';
+    div = document.createElement('div'),
+    cardsSource = 'cardTheme2.png';
 
-root.style.backgroundColor = 'blue';
-div.style.backgroundImage = 'url(' + cardsSourse + ')';
-div.style.backgroundPosition = '';
+root.id = 'root';
 
+div.style.position = 'absolute';
+div.style.left = (document.body.clientWidth - 210) / 2 + 'px';
+div.style.top = (document.body.clientHeight - 300) / 2 + 'px';
+div.style.backgroundImage = 'url(' + cardsSource + ')';
+div.style.backgroundPosition = (getRandomNumber(0, 8) | 0) * 210 + 'px '
+    + (getRandomNumber(0, 4) | 0) * 300 + 'px';
+div.style.width = '210px';
+div.style.height = '300px';
+
+root.appendChild(div);
 document.body.insertBefore(root, document.body.firstChild);
