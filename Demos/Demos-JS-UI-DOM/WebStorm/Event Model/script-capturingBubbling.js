bec91c16610949318060b90a95dacc4dd79a6bd3
Ivaylo <i.kostov86@gmail.com>
2015-10-13 00:59:17 +0300

    event model demo

--- Demos/Demos-JS-UI-DOM/WebStorm/Event Model/script-capturingBubbling.js
+++ Demos/Demos-JS-UI-DOM/WebStorm/Event Model/script-capturingBubbling.js
@@ -1,5 +1,6 @@
 var ul,
     li,
+    span,
     i,
     target;
 
@@ -12,12 +13,14 @@ for (i = 0; i < 5; i += 1) {
     ul.appendChild(li.cloneNode(true));
 }
 
+span = document.createElement('span');
+ul.appendChild(span);
+
 ul.addEventListener('click', function (ev) {
     target = ev.target;
 
     if (ev.target.className.indexOf('list-item') >= 0) {
-        console.log(this);
-        console.log(ev.target.innerHTML);
+        span.innerHTML = this.tagName + ': ' + ev.target.innerHTML;
     }
 });
 
