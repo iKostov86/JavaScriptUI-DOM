4e963048143293b891af00a84ca472ba5ca48a80
Ivaylo <i.kostov86@gmail.com>
2015-10-15 00:39:36 +0300

    jquery2014

---
+++ Demos/Demos-JS-UI-DOM/VisualStudio/jQuery/jQuery/scripts/jQueryZoomPlugin.js
@@ -0,0 +1,32 @@
+﻿$.fn.zoom = function () {
+    var $this = $(this.selector);
+
+    $this.on('mouseover', function (ev) {
+        var $this = $(this);
+
+        //return string - '100px'
+        //var width = $this.css('width');
+        //var height = $this.css('height');
+
+        //return value - 100
+        var w = $this.width();
+        var h = $this.height();
+
+        $this.width(w * 2);
+        $this.height(h * 2);
+        $this.css({ color: 'yellow' });
+    });
+
+    $this.on('mouseout', function (ev) {
+        var $this = $(this);
+
+        var w = $this.width();
+        var h = $this.height();
+
+        $this.width(w / 2);
+        $this.height(h / 2);
+        $this.css({color: 'transparent'});
+    });
+
+    return $this;
+};
