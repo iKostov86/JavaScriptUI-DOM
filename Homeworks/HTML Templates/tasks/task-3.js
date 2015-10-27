991b53e1327a9adf15e7cf53fef5e55457e6e289
iKostov86 <i.kostov86@gmail.com>
2015-10-27 02:24:47 +0200

    HandlebarsJS homework is done

--- Homeworks/HTML Templates/tasks/task-3.js
+++ Homeworks/HTML Templates/tasks/task-3.js
@@ -3,13 +3,19 @@ function solve() {
         $.fn.listview = function (data) {
             var $this = this;
             var dateAttr = $this.data('template');
-            var htmlTemplate = document.getElementById(dateAttr).innerHTML;
-            var template = handlebars.compile(htmlTemplate);
+            var htmlTemplate = $('#' + dateAttr).html();
+            var template = '{{#each this}}' + htmlTemplate + '{{/each}}';
 
-            for (var i = 0; i < data.length; i += 1) {
-                var result = template(data[i]);
-                $this.append(result);
-            }
+            //var template = handlebars.compile(htmlTemplate);
+            var compileTemplate = handlebars.compile(template);
+
+            //for (var i = 0; i < data.length; i += 1) {
+            //    var result = template(data[i]);
+            //    $this.append(result);
+            //}
+
+            var result = compileTemplate(data);
+            $this.append(result);
 
             return this;
         };
