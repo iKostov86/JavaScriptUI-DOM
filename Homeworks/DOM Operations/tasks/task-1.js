2a79fb29b43c5f3b3e9b80e76fe39f1442e03760
iKostov86 <i.kostov86@gmail.com>
2015-10-29 01:03:05 +0200

    dom operations with jquery

--- Homeworks/DOM Operations/tasks/task-1.js
+++ Homeworks/DOM Operations/tasks/task-1.js
@@ -17,39 +17,81 @@ Create a function that takes an id or DOM element and an array of contents
     * In that case, the content of the element **must not be** changed   
 */
 
+//module.exports = function () {
+//
+//    return function (element, contents) {
+//        if (typeof element === 'undefined') {
+//            throw Error;
+//        } else if (typeof element === 'string') {
+//            element = document.getElementById(element);
+//            if (!element) {
+//                throw Error;
+//            }
+//        } else if (!(element instanceof HTMLElement) && element !== null) {
+//            throw Error;
+//        }
+//
+//        if (typeof contents === 'undefined') {
+//            throw Error;
+//        }
+//
+//        if (contents.some(function (item) {
+//                return typeof item !== 'string' && typeof item !== 'number';
+//            })) {
+//            throw Error;
+//        }
+//
+//        element.innerHTML = '';
+//
+//        var div = document.createElement('div');
+//
+//        for (var i = 0, length = contents.length; i < length; i += 1) {
+//            div = div.cloneNode(true);
+//            div.innerHTML = contents[i];
+//
+//            element.appendChild(div);
+//        }
+//    }
+//};
+
 module.exports = function () {
 
-  return function (element, contents) {
-      if (typeof element === 'undefined') {
-          throw Error;
-      } else if (typeof element === 'string') {
-          element = document.getElementById(element);
-          if (!element) {
-              throw Error;
-          }
-      } else if (!(element instanceof HTMLElement) && element !== null) {
-          throw Error;
-      }
+    return function (element, contents) {
+        var $element,
+            $contents;
 
-      if (typeof contents === 'undefined') {
-          throw Error;
-      }
+        if (typeof element === 'string') {
+            $element = $('#' + element);
+        } else if (element instanceof HTMLElement) {
+            $element = $(element);
+        } else {
+            throw Error;
+        }
 
-      if (contents.some(function (item) {
-              return typeof item !== 'string' && typeof item !== 'number';
-          })) {
-          throw Error;
-      }
+        if (!$element.length) {
+            throw Error;
+        }
 
-      element.innerHTML = '';
+        if (contents) {
+            $contents = $(contents);
+        } else {
+            throw Error;
+        }
 
-      var div  = document.createElement('div');
+        if ($.grep($contents, function (item) {
+                return $.type(item) !== 'string' && $.type(item) !== 'number';
+            }).length > 0) {
+            throw Error;
+        }
 
-      for (var i = 0, length = contents.length; i < length; i += 1) {
-          div = div.cloneNode(true);
-          div.innerHTML = contents[i];
+        $element.html('');
 
-          element.appendChild(div);
-      }
-  };
+        var $div  = $('<div>');
+
+        for (var i = 0, length = $contents.length; i < length; i += 1) {
+            $div = $div.clone(true);
+            $div.html($contents[i]);
+            $element.append($div);
+        }
+    }
 };
