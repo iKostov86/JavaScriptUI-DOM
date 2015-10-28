a225fac75de2c5b6831837aee677e23eb4b66a7c
iKostov86 <i.kostov86@gmail.com>
2015-10-28 23:56:58 +0200

    event model with vanilla javascript and jquery

--- Homeworks/Event Model/tasks/task-1.js
+++ Homeworks/Event Model/tasks/task-1.js
@@ -2,29 +2,102 @@
 
 /* Create a function that takes an id or DOM element and: */
 
+//function solve() {
+//    return function (selector) {
+//        var root;
+//
+//        if (typeof selector === 'string') {
+//            root = document.getElementById(selector);
+//        } else if (selector instanceof HTMLElement) {
+//            root = selector;
+//        } else {
+//            throw Error;
+//        }
+//
+//        if (!root) {
+//            throw Error;
+//        }
+//
+//        var btns = root.getElementsByClassName('button');
+//        var contents = root.getElementsByClassName('content');
+//
+//        for (var i = 0; i < btns.length; i += 1) {
+//            btns[i].innerHTML = 'hide';
+//        }
+//
+//        root.addEventListener('click', function (ev) {
+//            var target = ev.target;
+//
+//            if (target.className === 'button') {
+//                var nextElement = target.nextElementSibling;
+//
+//                while (nextElement) {
+//                    if (nextElement.className === 'content') {
+//                        if (nextElement.style.display === 'none') {
+//                            nextElement.style.display = '';
+//                            target.innerHTML = 'hide';
+//                        } else {
+//                            nextElement.style.display = 'none';
+//                            target.innerHTML = 'show';
+//                        }
+//
+//                        break;
+//                    } else if (nextElement.className === 'button') {
+//                        break;
+//                    }
+//
+//                    nextElement = nextElement.nextElementSibling;
+//                }
+//            }
+//        });
+//    };
+//}
+
 function solve() {
     return function (selector) {
-        if (typeof selector !== 'string') {
+        var $root;
+
+        if (typeof selector === 'string') {
+            $root = $('#' + selector);
+        } else if (selector instanceof HTMLElement) {
+            $root = $(selector);
+        } else {
             throw Error;
         }
 
-        var root = document.getElementById(selector);
-
-        if (!root) {
+        if (!$root.length) {
             throw Error;
         }
 
-        var btns = root.getElementsByClassName('button');
+        var $btns = $root.find('.button');
 
-        for (var i = 0; i < btns.length; i += 1) {
-            btns[i].innerHTML = 'hide';
-            btns[i].addEventListener('click', function () {
-                
-            });
-        }
-        var contents = root.getElementsByClassName('content');
+        $btns.html('hide');
 
+        $root.on('click', function (ev) {
+            var $target = $(ev.target);
 
+            if ($target.hasClass('button')) {
+                var $nextElement = $target.next();
+
+                while ($nextElement) {
+                    if ($nextElement.hasClass('content')) {
+                        if ($nextElement.css('display') === 'none') {
+                            $nextElement.css('display', '');
+                            $target.html('hide');
+                        } else {
+                            $nextElement.css('display', 'none');
+                            $target.html('show');
+                        }
+
+                        break;
+                    } else if ($nextElement.hasClass('button')) {
+                        break;
+                    }
+
+                    $nextElement = $nextElement.next();
+                }
+            }
+        });
     };
 }
 
