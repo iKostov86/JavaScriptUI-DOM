a6f4c4c198fa51b514b1b8814cd36b26126c15c0
iKostov86 <i.kostov86@gmail.com>
2015-10-28 14:06:44 +0200

    tasks 2014

---
+++ Homeworks/DOM Operations/tasks_2014/script-create-divs.js
@@ -0,0 +1,27 @@
+(function (number) {
+    var rootStyle = window.getComputedStyle(root),
+        strong = document.createElement('strong');
+
+    strong.innerHTML = 'div';
+
+    for (var i = 0; i < number; i += 1) {
+        var div = document.createElement('div');
+
+        div.style.width = getRandomNumber(20, 100) + 'px';
+        div.style.height = getRandomNumber(20, 100) + 'px';
+        div.style.background = getRandomColor();
+        div.style.border = getRandomNumber(1, 20) + 'px solid ' + getRandomColor();
+        div.style.borderRadius = getRandomNumber(1, 25) + 'px';
+        div.style.fontcolor = getRandomColor();
+        div.style.position = 'absolute';
+        div.style.left = getRandomNumber(parseFloat(rootStyle.getPropertyValue('border-width')),
+                document.body.clientWidth - parseFloat(rootStyle.getPropertyValue('border-width')) - parseFloat(div.style.width) - parseFloat(div.style.borderWidth) * 2)
+            + 'px';
+        div.style.top = getRandomNumber(parseFloat(rootStyle.getPropertyValue('border-width')),
+                document.body.clientHeight - parseFloat(rootStyle.getPropertyValue('border-width')) - parseFloat(div.style.height) - parseFloat(div.style.borderWidth) * 2)
+            + 'px';
+
+        div.appendChild(strong.cloneNode(true));
+        root.appendChild(div);
+    }
+}(getRandomNumber(5, 8)));
