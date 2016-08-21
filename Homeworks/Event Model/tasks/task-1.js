20e49448d822b1ea414b7480cc7cc0b316750e0c
iKostov86 <i.kostov86sync@gmail.com>
2016-08-21 13:54:45 +0300

    some improvements

--- Homeworks/Event Model/tasks/task-1.js
+++ Homeworks/Event Model/tasks/task-1.js
@@ -1,104 +1,116 @@
 /* globals $ */
 
-/* Create a function that takes an id or DOM element and: */
+// Create a function that takes an id or DOM element and:
 
-//function solve() {
-//    return function (selector) {
-//        var root;
-//
-//        if (typeof selector === 'string') {
-//            root = document.getElementById(selector);
-//        } else if (selector instanceof HTMLElement) {
-//            root = selector;
-//        } else {
-//            throw Error;
-//        }
-//
-//        if (!root) {
-//            throw Error;
-//        }
-//
-//        var btns = root.getElementsByClassName('button');
-//        var contents = root.getElementsByClassName('content');
-//
-//        for (var i = 0; i < btns.length; i += 1) {
-//            btns[i].innerHTML = 'hide';
-//        }
-//
-//        root.addEventListener('click', function (ev) {
-//            var target = ev.target;
-//
-//            if (target.className === 'button') {
-//                var nextElement = target.nextElementSibling;
-//
-//                while (nextElement) {
-//                    if (nextElement.className === 'content') {
-//                        if (nextElement.style.display === 'none') {
-//                            nextElement.style.display = '';
-//                            target.innerHTML = 'hide';
-//                        } else {
-//                            nextElement.style.display = 'none';
-//                            target.innerHTML = 'show';
-//                        }
-//
-//                        break;
-//                    } else if (nextElement.className === 'button') {
-//                        break;
-//                    }
-//
-//                    nextElement = nextElement.nextElementSibling;
-//                }
-//            }
-//        });
-//    };
-//}
+//     If an id is provided, select the element
+//     Finds all elements with class button or content within the provided element
+//         Change the content of all .button elements with "hide"
+//     When a .button is clicked:
+//         Find the topmost .content element, that is before another .button and:
+//             If the .content is visible:
+//                 Hide the .content
+//                 Change the content of the .button to "show"
+//             If the .content is hidden:
+//                 Show the .content
+//                 Change the content of the .button to "hide"
+//             If there isn't a .content element after the clicked .button and before other .button, do nothing
+//     Throws if:
+//         The provided DOM element is non-existant
+//         The id is neither a string nor a DOM element
+
 
 function solve() {
-    return function (selector) {
-        var $root;
+   return function (selector) {
+       var root;
 
-        if (typeof selector === 'string') {
-            $root = $('#' + selector);
-        } else if (selector instanceof HTMLElement) {
-            $root = $(selector);
-        } else {
-            throw Error;
-        }
+       if (typeof selector === 'string') {
+           root = document.getElementById(selector);
+       } else if (selector instanceof HTMLElement && selector) {
+           root = selector;
+       } else {
+           throw Error;
+       }
 
-        if (!$root.length) {
-            throw Error;
-        }
+       var btns = root.getElementsByClassName('button');
+       var contents = root.getElementsByClassName('content');
 
-        var $btns = $root.find('.button');
+       for (var i = 0; i < btns.length; i += 1) {
+           btns[i].innerHTML = 'hide';
+       }
 
-        $btns.html('hide');
+       root.addEventListener('click', function (ev) {
+           var target = ev.target;
 
-        $root.on('click', function (ev) {
-            var $target = $(ev.target);
+           if (target.className === 'button') {
+               var nextElement = target.nextElementSibling;
 
-            if ($target.hasClass('button')) {
-                var $nextElement = $target.next();
+               while (nextElement) {
+                   if (nextElement.className === 'content') {
+                       if (nextElement.style.display === 'none') {
+                           nextElement.style.display = '';
+                           target.innerHTML = 'hide';
+                       } else {
+                           nextElement.style.display = 'none';
+                           target.innerHTML = 'show';
+                       }
+                       break;
+                   } else if (nextElement.className === 'button') {
+                       break;
+                   }
 
-                while ($nextElement) {
-                    if ($nextElement.hasClass('content')) {
-                        if ($nextElement.css('display') === 'none') {
-                            $nextElement.css('display', '');
-                            $target.html('hide');
-                        } else {
-                            $nextElement.css('display', 'none');
-                            $target.html('show');
-                        }
-
-                        break;
-                    } else if ($nextElement.hasClass('button')) {
-                        break;
-                    }
-
-                    $nextElement = $nextElement.next();
-                }
-            }
-        });
-    };
+                   nextElement = nextElement.nextElementSibling;
+               }
+           }
+       });
+   };
 }
+
+// function solve() {
+//     return function (selector) {
+//         var $root;
+
+//         if (typeof selector === 'string') {
+//             $root = $('#' + selector);
+//         } else if (selector instanceof HTMLElement) {
+//             $root = $(selector);
+//         } else {
+//             throw Error;
+//         }
+
+//         if (!$root.length) {
+//             throw Error;
+//         }
+
+//         var $btns = $root.find('.button');
+
+//         $btns.html('hide');
+
+//         $root.on('click', function (ev) {
+//             var $target = $(ev.target);
+
+//             if ($target.hasClass('button')) {
+//                 var $nextElement = $target.next();
+
+//                 while ($nextElement) {
+//                     if ($nextElement.hasClass('content')) {
+//                         if ($nextElement.css('display') === 'none') {
+//                             $nextElement.css('display', '');
+//                             $target.html('hide');
+//                         } else {
+//                             $nextElement.css('display', 'none');
+//                             $target.html('show');
+//                         }
+
+//                         break;
+//                     } else if ($nextElement.hasClass('button')) {
+//                         break;
+//                     }
+
+//                     $nextElement = $nextElement.next();
+//                 }
+//             }
+//         });
+//     };
+// }
 
 module.exports = solve;
